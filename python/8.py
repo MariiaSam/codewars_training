57cbc5b746d0b7109e0d30b02254a3c9e83cffea
MariiaSam <kontragent@ukr.net>
2024-06-14 19:51:59 +0300

    add others var

--- python/8.py
+++ python/8.py
@@ -48,3 +48,25 @@
 5.Повернення рез-ту
 
 """
+"""
+from others, diff options
+def past(h, m, s):
+    if 0 <= h <= 23 or 0 <= m <= 59 or 0 <= s <= 59:
+        x = h * 3600000
+        y = m * 60000
+        z = s * 1000
+        total = x + y + z
+        return(total)
+    else:
+        return("Please input an hour between 0 and 23 and a minute or second inbetween 0 and 59.")
+
+        2 opt
+ def past(h, m, s):
+    return ((h * 60 + m) * 60 + s) * 1000    
+
+   3 opt 
+ from datetime import timedelta
+
+def past(h, m, s):
+    return timedelta(hours=h, minutes=m, seconds=s) // timedelta(milliseconds=1)      
+"""
