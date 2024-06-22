45ff9529af2108308e1c6b795823be594aa08a82
MariiaSam <kontragent@ukr.net>
2024-06-22 18:37:58 +0300

    add tasks

---
+++ python/hackerrank.py
@@ -0,0 +1,29 @@
+# 2
+# n = int(input())
+
+# if n % 2 == 0 and range(2, 5):
+#     print("Not Weird")
+# elif n % 2 == 0 and range(6, 20):
+#     print("Weird")
+# elif n % 2 == 0 and n > 20:
+#     print("Not Weird")
+# elif n % 2 == 1:
+#     print('Weird')
+# else:
+#     print("Weird")
+
+
+#===================================
+# 2
+# n = int(input())
+
+
+# if n % 2 != 0:  
+#     print("Weird")
+# elif 2 <= n <= 5:  
+#     print("Not Weird")
+# elif 6 <= n <= 20:  
+#     print("Weird")
+# else:  
+#     print("Not Weird")
+
