3d6c23ba496b3e7828943cf4bc41459a565b4093
MariiaSam <kontragent@ukr.net>
2024-06-25 19:10:51 +0300

    tasks hackerrank

--- python/hackerrank.py
+++ python/hackerrank.py
@@ -13,17 +13,40 @@
 #     print("Weird")
 
 
-#===================================
+# ===================================
 # 2
 # n = int(input())
 
 
-# if n % 2 != 0:  
+# if n % 2 != 0:
 #     print("Weird")
-# elif 2 <= n <= 5:  
+# elif 2 <= n <= 5:
 #     print("Not Weird")
-# elif 6 <= n <= 20:  
+# elif 6 <= n <= 20:
 #     print("Weird")
-# else:  
+# else:
 #     print("Not Weird")
+
+# ===================================
+# 3
+# if __name__ == '__main__':
+#     a = int(input())
+#     b = int(input()) 
+# print(a + b)
+# print(a - b)
+# print(a * b)
+
+# ===================================
+# 4
+# if __name__ == '__main__':
+#     a = int(input())
+#     b = int(input())
+# print(a//b)
+# print(a/b)
+# ===================================
+# 5
+# if __name__ == '__main__':
+#     n = int(input())
+# for i in range(0, n):
+#     print(i**2)
 
